210753951211839aa76dc3c2567523c3e8b7db9f
Aditya Neve <aditya.neve@gmail.com>
2021-12-05 23:05:11 +0530

    components updated

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
+require('dotenv').config();
 
 const Map = () => {
-    return(
-        <h1>Map</h1>
-    )
-}
+    const classes = useStyles();
+    const isMobile = useMediaQuery(`(min-width: 600px)`);
+    const coordinates = { lat: 0, lon: 0 };
 
-export default Map
+    return (
+        <div className={classes.mapContainer}>
+            {console.log(process.env.GOOGLE_MAPS_API_KEY)}
+
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key: process.env.GOOGLE_MAPS_API_KEY,
+                }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                // options={""}
+                // onChange={""}
+                // onChildClick={""}
+            ></GoogleMapReact>
+        </div>
+    );
+};
+
+export default Map;
